957704a59f34251d7af85e843052bfd0914146de
mango750 <whitemango8@gmail.com>
2023-12-19 14:23:37 +0900

    ex22

--- ex02/src/main/java/org/spring/controller/BoardController.java
+++ ex02/src/main/java/org/spring/controller/BoardController.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
+
+// 깃주석
 @Controller // 이거 컨트롤러얌
 @RequestMapping("/board/*") // board 접근하는 애들은 다 이 컨트롤러가 관리
 @AllArgsConstructor // 기본생성자 자동 호출(boardService주입)
